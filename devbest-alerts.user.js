833262fd421670147d2b923f20de135b32dae085
TesoMayn <tesomayn@gmail.com>
2015-12-13 14:43:14 -0500

    Version fix

--- devbest-alerts.user.js
+++ devbest-alerts.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DevBest Alerts
 // @namespace    http://tesomayn.com/
-// @version      1.5.2
+// @version      1.5.3
 // @description  Get Notifications for DevBest Shoutbox
 // @author       TesoMayn
 // @copyright    2015
